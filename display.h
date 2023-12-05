3541db69302ef2543b179a35fd3921510d03cfff
Iris Teyssier <iris.teyssier@gmail.com>
2023-12-04 16:17:46 -0800

    add basic groundwork

---
+++ display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_161004122023_H
+#define DISPLAY_161004122023_H
+
+#include <Arduino.h>
+
+//display class
+
+//will include conditional compilation based on an #define in main.h dictating whether there is a
+//backpack for the lcd
+
+#endif //DISPLAY_161004122023_H
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_160894122923_H
+#define MAIN_160894122923_H
+
+#include <Arduino.h>
+
+#endif //MAIN_160894122923_H
